c6366642ad6bb06c99dbb7d3bb9902e2f5377c99
scroitoru <58050635+scroitoru@users.noreply.github.com>
2021-12-26 00:49:03 -0500

    small change

--- src/main/java/MinesweeperApplication.java
+++ src/main/java/MinesweeperApplication.java
@@ -4,10 +4,12 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.Objects;
+
 public class MinesweeperApplication extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/minesweeper_application.fxml"));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/minesweeper_application.fxml")));
 
         Scene scene = new Scene(root, 800, 600);
 

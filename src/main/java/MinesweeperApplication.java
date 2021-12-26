b16cdcf82bd4a7a501e3ba8be924cf65009a8370
scroitoru <58050635+scroitoru@users.noreply.github.com>
2021-12-25 19:31:41 -0500

    application and controller created

--- src/main/java/MinesweeperApplication.java
+++ src/main/java/MinesweeperApplication.java
@@ -1,9 +1,18 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class MinesweeperApplication extends Application {
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/minesweeper_application.fxml"));
 
+        Scene scene = new Scene(root, 800, 600);
+
+        stage.setTitle("Minesweeper");
+        stage.setScene(scene);
+        stage.show();
     }
 }

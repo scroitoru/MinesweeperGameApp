d81d092e2444f61d2a8b035993424cac1da8d1a4
scroitoru <58050635+scroitoru@users.noreply.github.com>
2022-01-04 16:09:51 -0500

    remove comment

--- src/test/java/MinesweeperControllerTest.java
+++ src/test/java/MinesweeperControllerTest.java
@@ -54,7 +54,7 @@ public class MinesweeperControllerTest {
         //verify setOnMouseClicked
     }
 
-    @Test //incomplete
+    @Test
     public void flagCell(){
         //given
         givenMinesweeperController();
